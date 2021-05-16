ccc29535fc7a14ee2a3795bade98cd8451991d5f
aurele3000 <aurele.hurtis@gmail.com>
2021-05-16 23:53:12 +0200

    commit 16-05-21 minuit

---
+++ src/components/Actualites2.vue
@@ -0,0 +1,40 @@
+<template>
+   
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/media/card2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator color: #49ada1;">Prommenons-nous <br>dans les bois<i class="material-icons right">Ouvrir</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title color: #49ada1;">Dans les bois<i class="material-icons right">Fermer </i></span>
+      <p>C'est arrivé près de chez vous...</p>
+    </div>
+  </div> 
+</template>
+
+<style scoped>
+.card {
+    width: 400px;
+    height: auto;
+    float: left;
+    margin-left: 50px;
+    padding: 10px;
+}
+.card p {
+    font-size: 16px;
+    font-weight: 300;   
+}
+.card span {
+    font-family: 'Prompt', sans-serif;
+    font-size: 26px;
+    font-weight: 400;
+    color: #49ada1;   
+}
+.card i {
+    font-size: 16px;
+    font-weight: 300;   
+}
+</style>
--- src/views/Actualites.vue
+++ src/views/Actualites.vue
@@ -1,19 +1,47 @@
 <template>
-
-    <div class="Actualites">
-    <Actus/>
-    </div>
-
+  <div class="Actualites">
+    <h1>Actualités</h1>
+    <Actualites1 />
+    <Actualites2 />
+    <Actualites3 />
+  </div>
 </template>
 
 <script>
-
-import Actus from "../components/Actus.vue";
+import Actualites1 from "../components/Actualites1.vue";
+import Actualites2 from "../components/Actualites2.vue";
+import Actualites3 from "../components/Actualites3.vue";
 
 export default {
- name: 'Actualites',
-components: {
-  Actus
-  }
-}
+  name: "Actualites",
+  components: {
+    Actualites1,
+    Actualites2,
+    Actualites3,
+  },
+};
 </script>
+<style scoped>
+.Actualites {
+  width: 1440px;
+  height: 600px;
+  margin-right: auto;
+  margin-left: auto;
+  background-color: #fcf8f2;
+  font-family: "Prompt", sans-serif;
+
+}
+.Actualites h1{
+  font-size: 48px;
+  font-weight: 600;
+}
+
+.card {
+    width: 400px;
+    height: auto;
+    float: left;
+    margin-left: 50px;
+    padding: 10px;
+    margin-top: 50px;
+}
+</style>
